8f823571d3f7fcaaf827e42b53b936797da5090b
GasparVardanyan <gaspar.vardanyan.mailbox@gmail.com>
2019-01-02 00:50:16 +0400

    fix compile-win.sh

--- compile-win.sh
+++ compile-win.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 i686-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -o windows/gcrypt32.exe main.cpp console.h gcrypt.h
 x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -o windows/gcrypt64.exe main.cpp console.h gcrypt.h
