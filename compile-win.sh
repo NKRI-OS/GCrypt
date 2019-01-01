ddfce393c0fed133d5254523fcf5d78830a611cb
GasparVardanyan <gaspar.vardanyan.mailbox@gmail.com>
2019-01-02 00:20:49 +0400

    apply support for Microsoft Windows OS

---
+++ compile-win.sh
@@ -0,0 +1,2 @@
+i686-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -o windows/gcrypt32.exe main.cpp console.h gcrypt.h
+x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -o windows/gcrypt64.exe main.cpp console.h gcrypt.h
